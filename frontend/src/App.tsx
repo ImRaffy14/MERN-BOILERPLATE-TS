95fa323d7149950cf93408fae6f9baa0f49468c1
ImRaffy14 <raffysolis123@gmail.com>
2025-04-24 17:32:48 +0800

    fix: reorder JSX elements in App component for improved readability

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -10,17 +10,10 @@ import { Toaster } from "react-hot-toast"
 function App() {
 
   return (
-    <Routes>
-      <Route path="/test" element={<FullPageLoader/>}/>
-      <Route path="/login" element={<Login />} />
-      <Route path="/" element={<WithSocket><MainLayout /></WithSocket>}>
-        <Route index element={<Dashboard />} />
-        <Route path="/users" element={<UserManagement />} />
-      </Route>
-    </Routes>
     <>
       <Toaster position="top-center" reverseOrder={false} />
       <Routes>
+      <Route path="/test" element={<FullPageLoader/>}/>
         <Route path="/login" element={<Login />} />
         <Route path="/" element={<WithSocket><MainLayout /></WithSocket>}>
           <Route index element={<Dashboard />} />
